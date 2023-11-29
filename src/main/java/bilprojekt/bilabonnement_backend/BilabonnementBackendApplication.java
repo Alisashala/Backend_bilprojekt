49ff5b788d8b62e4822aecfa884c68912a1b2e1e
Alisashala <alis0216@tud.kea.dk>
2023-11-29 13:16:10 +0100

    first push

---
+++ src/main/java/bilprojekt/bilabonnement_backend/BilabonnementBackendApplication.java
@@ -0,0 +1,13 @@
+package bilprojekt.bilabonnement_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilabonnementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilabonnementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bilprojekt/bilabonnement_backend/BilabonnementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bilprojekt.bilabonnement_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilabonnementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
